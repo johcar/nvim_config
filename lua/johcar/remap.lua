08039382edb8972e37f98f8c17d3beb194b01105
johcar <johancarlstrom@gmail.com>
2024-06-10 19:39:16 +0200

    Add remap for jumping between splits

--- lua/johcar/remap.lua
+++ lua/johcar/remap.lua
@@ -7,5 +7,11 @@ vim.keymap.set("n", "<leader>pe", ":NvimTreeToggle<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- jump between splits
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
 -- move line to this line without moving cursor(which is default)
 vim.keymap.set("n", "J", "mzJ`z")
